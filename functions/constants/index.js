a7be200435b44a8cb2d30b14280cb85014d272a0
Huy Nguyen <huyb.1991@gmail.com>
2022-05-11 12:34:00 +0700

    Update entity

--- functions/constants/index.js
+++ functions/constants/index.js
@@ -3,14 +3,14 @@ BASE_URL_ADMIN = 'http://localhost:5000';
 BASE_URL_SITEMAP = 'https://authenticaftertaste.com';
 
 const ENTITY = {
-  COUNTRY: 'country',
+  CUISINE: 'cuisine',
   RECIPE: 'recipe',
   BLOG: 'blog',
   SITEMAP: 'sitemap'
 };
 
 const ENTITY_NAME = {
-  [ENTITY.COUNTRY]: 'Country',
+  [ENTITY.CUISINE]: 'Cuisine',
   [ENTITY.RECIPE]: 'Recipe',
 };
 
