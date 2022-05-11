743172c4d631c7eb0f4127d3a336cc1a563ec6b0
Huy Nguyen <huyb.1991@gmail.com>
2022-05-11 17:28:05 +0700

    Define recipe category

--- functions/constants/index.js
+++ functions/constants/index.js
@@ -24,6 +24,22 @@ const RATING_SCORE = {
   MAX: 5,
 };
 
+const RECIPE_CATEGORY = [
+  { name: 'Breakfast (bữa sáng)', slug: 'breakfast' },
+  { name: 'Lunch (bữa trưa)', slug: 'lunch' },
+  { name: 'Dinner (bữa tối)', slug: 'dinner' },
+  { name: 'Appetizer (khai vị)', slug: 'appetizer' },
+  { name: 'Salad', slug: 'salad' },
+  { name: 'Main-course (món chính)', slug: 'main-course' },
+  { name: 'Side-dish (món phụ)', slug: 'side-dish' },
+  { name: 'Baked-goods (đồ nướng)', slug: 'baked-goods' },
+  { name: 'Dessert (tráng miệng)', slug: 'dessert' },
+  { name: 'Snack (qua loa)', slug: 'snack' },
+  { name: 'Soup (canh)', slug: 'soup' },
+  { name: 'Holiday (ngày lễ)', slug: 'holiday' },
+  { name: 'Vegetarian (món chay)', slug: 'vegetarian' },
+];
+
 module.exports = {
   BASE_URL,
   BASE_URL_ADMIN,
@@ -32,4 +48,5 @@ module.exports = {
   ENTITY_NAME,
   CATEGORY_SLUG,
   RATING_SCORE,
+  RECIPE_CATEGORY,
 };
