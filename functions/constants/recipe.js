849583bb591de605845956e029414fa7bd89a503
Huy Nguyen <huyb.1991@gmail.com>
2022-05-11 22:09:09 +0700

    Update cuisine

--- functions/constants/recipe.js
+++ functions/constants/recipe.js
@@ -1,18 +1,19 @@
 // Refer: https://www.csmonitor.com/The-Culture/Food/Stir-It-Up/Topics
 // Other: https://www.recipetineats.com/categories/
 const RECIPE_CATEGORY = [
-  { name: 'Breakfast (bữa sáng)', slug: 'breakfast' },
-  { name: 'Lunch (bữa trưa)', slug: 'lunch' },
-  { name: 'Dinner (bữa tối)', slug: 'dinner' },
   { name: 'Appetizer (khai vị)', slug: 'appetizer' },
-  { name: 'Salad', slug: 'salad' },
-  { name: 'Main-course (món chính)', slug: 'main-course' },
-  { name: 'Side-dish (món phụ)', slug: 'side-dish' },
+  { name: 'Breakfast (bữa sáng)', slug: 'breakfast' },
   { name: 'Baked-goods (đồ nướng)', slug: 'baked-goods' },
+  { name: 'Lunch (bữa trưa)', slug: 'lunch' },
+  { name: 'Drink (đồ uống)', slug: 'drink' },
+  { name: 'Dinner (bữa tối)', slug: 'dinner' },
   { name: 'Dessert (tráng miệng)', slug: 'dessert' },
+  { name: 'Main-course (món chính)', slug: 'main-course' },
+  { name: 'Holiday (ngày lễ)', slug: 'holiday' },
+  { name: 'Salad', slug: 'salad' },
+  { name: 'Side-dish (món phụ)', slug: 'side-dish' },
   { name: 'Snack (qua loa)', slug: 'snack' },
   { name: 'Soup (canh)', slug: 'soup' },
-  { name: 'Holiday (ngày lễ)', slug: 'holiday' },
   { name: 'Vegetarian (món chay)', slug: 'vegetarian' },
 ];
 
