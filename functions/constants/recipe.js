4ef1d171558171a7048f8eacfb6043054cad5b4d
Huy Nguyen <huyb.1991@gmail.com>
2022-05-11 18:34:12 +0700

    Define recipe cuisine

--- functions/constants/recipe.js
+++ functions/constants/recipe.js
@@ -16,6 +16,186 @@ const RECIPE_CATEGORY = [
   { name: 'Vegetarian (món chay)', slug: 'vegetarian' },
 ];
 
+// Refer: https://food.ndtv.com/recipes/by-cuisine
+const RECIPE_CUISINE = [
+  {
+    name: 'African',
+    slug: 'african',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'American',
+    slug: 'American',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Asian',
+    slug: 'Asian',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Caribbean',
+    slug: 'Caribbean',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Chinese',
+    slug: 'Chinese',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Continental',
+    slug: 'Continental',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Cuban',
+    slug: 'Cuban',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'European',
+    slug: 'European',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'French',
+    slug: 'French',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Greek',
+    slug: 'Greek',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Indian',
+    slug: 'Indian',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Indonesian',
+    slug: 'Indonesian',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Italian',
+    slug: 'Italian',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Japanese',
+    slug: 'Japanese',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Korean',
+    slug: 'Korean',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Lebanese',
+    slug: 'Lebanese',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Malaysian',
+    slug: 'Malaysian',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Mexican',
+    slug: 'Mexican',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Pakistani',
+    slug: 'Pakistani',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Russian',
+    slug: 'Russian',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Singapore',
+    slug: 'Singapore',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Spanish',
+    slug: 'Spanish',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Thai',
+    slug: 'Thai',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Tibetan',
+    slug: 'Tibetan',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+  {
+    name: 'Vietnamese',
+    slug: 'Vietnamese',
+    img: '',
+    seoTitle: '',
+    seoDesc: ''
+  },
+];
+
 module.exports = {
   RECIPE_CATEGORY,
+  RECIPE_CUISINE,
 };
